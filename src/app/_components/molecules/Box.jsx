76ea0c2fd2c66febed4ad60e83dd6894066892df
von-ai <kuhhakuzuhdi@gmail.com>
2023-12-17 21:36:22 +0700

    refactor : 3 sub-hero

--- src/app/_components/molecules/Box.jsx
+++ src/app/_components/molecules/Box.jsx
@@ -1,11 +1,12 @@
-const Box = () => {
+const Box = ({children}) => {
   return (
-    <main className="flex">
-      <section className="relative w-[350px] h-[230px] rounded-[20px] border border-stone-300"> 
-        <div className="absolute w-0 h-[230px] border border-stone-300 left-14 overflow-x-hidden"></div>
-        <div className="absolute w-[349px] h-0 border border-stone-300 top-14 overflow-hidden"></div>
-        <div className="absolute w-[290px] h-[50px] border rounded-[20px] bg-slate-100 top-8 left-10"></div>
+    <main>
+      <section className="flex relative w-[400px] h-[230px] rounded-[20px] border border-stone-300"> 
+        <div className="absolute w-full top-[22%] border border-stone-300 "></div>
+        <div className="absolute h-full left-[14%] border border-stone-300"></div>
+        <div className="absolute w-[340px] h-[50px] border rounded-[20px] bg-slate-100 top-7 left-10"></div>
       </section>
+      {children}
     </main>
   )
 } 
--- src/app/_components/organisms/SubHero.jsx
+++ src/app/_components/organisms/SubHero.jsx
@@ -7,9 +7,10 @@ const SubHero = () => {
   return(
     <main className="flex flex-col">
       {/* membuat header KURANG PADDING*/}
-      <section className="text-primary-orange text-center font-bold text-heading-l  lg:text-heading-xl"> 
-        <h1>
-          LOREM IPSUM
+      <section className="text-primary-orange text-center font-bold text-heading-l pb-5 pt-5  
+      lg:text-heading-xl"> 
+        <h1 className="uppercase">
+          informasi umum
         </h1>
       </section>
 
@@ -19,8 +20,8 @@ const SubHero = () => {
           <Box className="relative flex">
             <section>
               <div className="absolute 
-                top-[9.5%] left-[13%]
-                lg:top-[72px] lg:left-[2%]
+                top-[15.5%] left-[13%]
+                lg:top-[115px] lg:left-[2%]
                 ">
                 <Circle bgColor="bg-secondary-teal">
                   <Image src="./icons/vectorGreen.svg" width={50} height={50} 
@@ -30,14 +31,14 @@ const SubHero = () => {
             </section>
             <section className="flex">
               <div className="absolute text-lg text-black font-medium
-                top-[13%] left-[28%]
-                lg:left-[9%] lg:top-[99px]
+                top-[20%] left-[28%]
+                lg:left-[9%] lg:top-[140px]
                 ">
                 Sejarah Yang Memukau
               </div>
               <div className="absolute w-[290px] text-base text-justify
-                top-[21%] left-[25%]
-                lg:top-[24%] lg:left-[8%]">
+                top-[28%] left-[25%]
+                lg:top-[30%] lg:left-[8%]">
                 Fort Rotterdam adalah peninggalan 
                 sejarah yang hidup, mencakup lebih 
                 dari tiga abad peristiwa penting 
@@ -51,8 +52,8 @@ const SubHero = () => {
           <Box className="relative flex">
             <section>
               <div className="absolute 
-              top-[49%] left-[13%]
-              lg:top-[72px] lg:left-[35.5%]">
+              top-[55%] left-[13%]
+              lg:top-[115px] lg:left-[35.5%]">
                 <Circle bgColor="bg-secondary-maroon">
                   <Image src="./icons/vectorMaroon.svg" width={60} height={60}
                     alt="maroon"/>
@@ -61,13 +62,13 @@ const SubHero = () => {
             </section>
             <section className="flex">
               <div className="absolute text-lg text-black font-medium
-                left-[28%] top-[52.5%]
-                lg:left-[42.5%] lg:top-[99px]">
+                left-[28%] top-[59.5%]
+                lg:left-[42.5%] lg:top-[140px]">
                 Koleksi Seni dan Budaya
               </div>
               <div className="absolute w-[290px] text-base text-justify
-                left-[25%] top-[60.5%]
-                lg:top-[24%] lg:left-[41.2%]">
+                left-[25%] top-[67.5%]
+                lg:top-[30%] lg:left-[41.2%]">
                 Dari seni lokal hingga artefak 
                 bersejarah, fort ini menyimpan 
                 kekayaan seni dan budaya yang 
@@ -81,8 +82,8 @@ const SubHero = () => {
           <Box className="relative flex">
             <section>
               <div className="absolute 
-                left-[13%] top-[88.5%]
-                lg:top-[72px] lg:left-[68.8%]">
+                left-[13%] top-[95%]
+                lg:top-[115px] lg:left-[68.8%]">
                 <Circle bgColor="bg-secondary-navy">
                   <Image src="./icons/vectorBlue.svg" width={60} height={60}
                     alt="blue"/>
@@ -91,13 +92,13 @@ const SubHero = () => {
             </section>
             <section className="flex">
               <div className="absolute text-lg text-black font-medium
-              left-[28%] top-[92.5%]
-              lg:left-[76%] lg:top-[99px]">
+              left-[28%] top-[99.5%]
+              lg:left-[76%] lg:top-[140px]">
                 Pengalaman Wisata Edukatif
               </div>
               <div className="absolute w-[290px] text-base text-justify
-              left-[25%] top-[100%]
-              lg:top-[24%] lg:left-[74.5%]">
+              left-[25%] top-[107%]
+              lg:top-[30%] lg:left-[74.5%]">
                 Fort Rotterdam menawarkan 
                 pengalaman wisata mendidik dan 
                 mengasyikkan yang mampu 
@@ -115,15 +116,15 @@ const SubHero = () => {
       {/* membuat kotak orange */}
       <section className="relative w-full h-[250px] md:h-[400px] lg:h-[500px] bg-primary-orange gap-3"> 
         <div>
-          <div className="absolute text-6xl top-20 left-[7%] text-white font-bold 
+          <div className="absolute text-6xl top-20 left-[5%] text-white font-bold 
             lg:left-[10%] lg:top-28 lg:text-9xl
             md:text-8xl md:top-32 md:left-[15%]">“</div>
-          <div className="absolute text-xl top-24 left-[17%] text-white font-bold leading-tight
+          <div className="absolute text-xl top-24 left-[13%] text-white font-bold leading-tight uppercase
             lg:text-5xl lg:top-44 lg:left-[17%] lg:leading-tight
             md:text-3xl md:top-40 md:left-[23%] md:leading-tight">
-          SEJARAH MENGUNGKAPKAN MASA LALU,<br/> 
-          MEMBERITAHU MASA KINI,<br/>
-          & MEMPERSIAPKAN MASA DEPAN
+              sejarah mengungkapkan masa lalu,<br/> 
+              memberitahu masa kini,<br/>
+              & mempersiapkan masa depan
           </div>
         </div>
       </section>
