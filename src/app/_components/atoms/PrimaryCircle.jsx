f44e2817b6813d0e816687916696fd3af0ad8d48
naddiyh <nadtss12@gmail.com>
2023-12-19 17:12:01 +0800

    Refactor : Primary Circle & Galeri

--- src/app/_components/atoms/PrimaryCircle.jsx
+++ src/app/_components/atoms/PrimaryCircle.jsx
@@ -1,10 +1,10 @@
 //Membuat Primary Circle untuk Tiap Icon
 const primary = (props) => {
-  const { children, bgColor } = props;
+  const { children, className } = props;
 
   return (
     <button
-      className={`flex rounded-full p-5 w-[80px] h-[80px] transition-transform justify-center items-center scale-[.65] md:scale-75 lg:scale-100  ${bgColor}`}
+      className={`flex rounded-full p-5 w-[80px] h-[80px] transition-transform justify-center items-center scale-[.65] md:scale-75 lg:scale-100 ${className}`}
     >
       {children}
     </button>
